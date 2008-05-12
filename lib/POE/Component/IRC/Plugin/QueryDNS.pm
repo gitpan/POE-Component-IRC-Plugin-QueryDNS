864fd7cc80f07213c6f939776b7ff67d7db6892d
Chris Williams <chris@bingosnet.co.uk>
2008-05-12 13:56:02 +0000

    Import of BINGOS/POE-Component-IRC-Plugin-QueryDNS-0.06 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-QueryDNS
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         BINGOS/POE-Component-IRC-Plugin-QueryDNS-0.06.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/QueryDNS.pm
+++ lib/POE/Component/IRC/Plugin/QueryDNS.pm
@@ -8,7 +8,7 @@ use POE::Component::IRC::Plugin qw(:ALL);
 use POE::Component::IRC::Common qw(irc_ip_is_ipv4);
 use vars qw($VERSION);
 
-$VERSION = '0.04';
+$VERSION = '0.06';
 
 sub new {
   my $package = shift;
@@ -225,6 +225,8 @@ Some examples:
 Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
 
 =head1 LICENSE
+
+Copyright C<(c)> Chris Williams.
 
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
